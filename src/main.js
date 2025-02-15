0e72cbe43745fcd69e3ed63dd806eba818044623
yaelren <yaelre@wix.com>
2025-02-15 14:02:48 +0200

    added model

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import './style.css'
 import * as THREE from 'three'
-import { addBoilerPlateMeshes, addStandardMesh, addTextureMesh,addMushroomMesh } from './addDefaultMeshes';
+import { addBoilerPlateMeshes, addStandardMesh, addTextureMesh, addMushroomMesh } from './addDefaultMeshes';
 import { addLight } from './addDefaultLights';
+import Model from './Model'
+
 
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 const clock = new THREE.Clock()
@@ -15,7 +17,7 @@ const camera = new THREE.PerspectiveCamera(
 const meshes = {}
 const lights = {}
 const scene = new THREE.Scene();
-
+const mixers = []
 
 
 init();
@@ -29,18 +31,36 @@ function init() {
   meshes.standard = addStandardMesh()
   meshes.physical = addTextureMesh()
   meshes.mushroom = addMushroomMesh()
+  meshes.physical.position.set(-2,-2,0)
+
 
   lights.default = addLight()
 
   scene.add(meshes.default)
   scene.add(meshes.standard)
-  scene.add(lights.default)
   scene.add(meshes.physical)
   scene.add(meshes.mushroom)
+  scene.add(lights.default)
 
   camera.position.set(0, 0, 5)
   resize();
+  instances()
   animate();
+}
+
+function instances() {
+  const flower = new Model({
+    name: 'flower',
+    scene: scene,
+    meshes: meshes,
+    url: 'flowers.glb',
+    scale: new THREE.Vector3(2, 2, 2),
+    position: new THREE.Vector3(0, -0.8, 3),
+    animationState: true,
+    mixers: mixers,
+    replace:true
+  })
+  flower.init()
 }
 
 function resize() {
@@ -52,17 +72,24 @@ function resize() {
 }
 
 function animate() {
-  const tick = clock.getElapsedTime()
+  // const tick = clock.getElapsedTime()
+  const delta = clock.getDelta()
+
   requestAnimationFrame(animate);
+  for (const mixer of mixers) {
+    mixer.update(delta)
+  }
+  if (meshes.flower) {
+    meshes.flower.rotation.y -= 0.001;
+  }
+  // meshes.physical.material.displacementScale = Math.sin(tick)
 
-  meshes.physical.material.displacementScale = Math.sin(tick)
 
-
-  meshes.default.rotation.x+=0.01
-  meshes.standard.rotation.y+=0.01
-  meshes.standard.rotation.z-=0.01
-  meshes.physical.rotation.y+=0.02
-  meshes.mushroom.rotation.x+=0.01
+  meshes.default.rotation.x += 0.01
+  meshes.standard.rotation.y += 0.01
+  meshes.standard.rotation.z -= 0.01
+  meshes.physical.rotation.y += 0.02
+  meshes.mushroom.rotation.x += 0.01
   // meshes.physical.rotation.x+=0.01
 
   renderer.render(scene, camera);
